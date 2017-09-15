f3bde2685de7500577940b69e4e378361cacf3fb
mmacatula <mmacatula@questnutrition.com>
2017-09-15 10:05:23 -0700

    Adding solution files

---
+++ MVC_test/MVC_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_test.Startup))]
+namespace MVC_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
